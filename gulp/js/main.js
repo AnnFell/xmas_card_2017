d7c094d68e5d6fd3420c6d9515942a00252aecae
AnnFell <annelottef@gmail.com>
2017-12-05 08:48:19 +0100

    Added launch animation

--- gulp/js/main.js
+++ gulp/js/main.js
@@ -3,6 +3,8 @@ $(document).ready(function () {
      //alert('jQuery werkt');
 
     var text, text2;
+    var factoryFlake = $('.snowflake');
+
 
     $('.test').on("click", function() {
         text = $(".text").val();
@@ -12,15 +14,21 @@ $(document).ready(function () {
     });
 
     //Launch
-    // $('.launch').on("click", function(e) {
-    //    console.log("klik launch");
-    // };
+    $('.launch').on("click", function() {
+
+       console.log("klik launch");
+       //TweenMax.killTweensOf('.snowflake');
+
+       var timeline = new TimelineMax();
+       timeline
+           .to($('.factory'), 1, ({marginTop:'10px', repeat:2, ease: Bounce.easeInOut, yoyo:true}))
+           .to(factoryFlake, 0.8, {top:'50px', repeat:2, ease: Bounce.easeInOut, yoyo:true})
+           .to(factoryFlake, 0.5, {top:'-1200px', ease: Power3.easeOut});
+       
+    });
 
     //Snowflake drift
-    var factoryFlake = $('.snowflake');
     TweenMax.to(factoryFlake, 2, {top:'-20px', repeat:-1, ease: Power1.easeInOut, yoyo:true});
-
-
 
 
 });
